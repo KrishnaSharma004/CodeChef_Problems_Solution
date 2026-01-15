5acdd8baabb1ce5d758a751b5b721e4bfd9cff98
Krishna Sharma <rakesh7355249204@gmail.com>
2026-01-15 14:48:43 +0530

    Fibonacci_Series

---
+++ Dynamic_programming/Fibonacci_Series.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int dp[110];
+
+long long int fib(int n) {
+    if(n==1) return 0;
+    if(n==2) return 1;
+    if(dp[n] != -1) return dp[n];
+    
+    return dp[n] = fib(n-1) + fib(n-2);
+}
+
+int main() {
+    memset(dp,-1,sizeof(dp));
+    int n;
+    cin >> n;
+    cout << fib(n) << endl;
+    return 0;
+}
